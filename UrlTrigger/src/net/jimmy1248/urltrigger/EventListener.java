eef6cbb6aa26148c16d246164289719f5ef8c10f
Jimmy Allen <jimmy1248@gmail.com>
2013-11-10 22:29:04 +1300

    A Commit

--- UrlTrigger/src/net/jimmy1248/urltrigger/EventListener.java
+++ UrlTrigger/src/net/jimmy1248/urltrigger/EventListener.java
@@ -14,22 +14,26 @@ public class EventListener implements Listener{
 		this.plugin = plugin;
 		url = plugin.getConfig().getString("url");
 	}
+	
+
 	@EventHandler
 	public void EnderDragonDeath(EntityDeathEvent event){
 		if(event.getEntity().equals(EntityType.ENDER_DRAGON)){
-			new SendUrlTrigger(url+"dragondeath");
+			new SendUrlTrigger(url+"?dragondeath="+event.getEntity().getKiller().getDisplayName());
+			plugin.getLogger().info(event.getEntity().getKiller()+" killed the ender dragon");
 		}
 		
 	}
 	
 	@EventHandler
 	public void PlayerJoinEvent(PlayerLoginEvent event){
-		new SendUrlTrigger(url+"join="+event.getPlayer().getDisplayName());
-		
+		new SendUrlTrigger(url+"?join="+event.getPlayer().getDisplayName());
+		plugin.getLogger().info(event.getPlayer().getPlayer().getDisplayName()+" joined");
 	}
 	
 	@EventHandler
 	public void PlayerLeaveEvent(PlayerQuitEvent event){
-		new SendUrlTrigger(url+"quit="+event.getPlayer().getDisplayName());
+		new SendUrlTrigger(url+"?quit="+event.getPlayer().getDisplayName());
+		plugin.getLogger().info(event.getPlayer().getPlayer().getDisplayName()+" quit");
 	}
 }
